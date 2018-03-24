7a9702a3a77562a871a3a1f0abfc706af83a2470
Carl Caum <carl@carlcaum.com>
2018-03-24 09:16:34 -0700

    Support timezones in log messages

--- controller/logger.js
+++ controller/logger.js
@@ -1,18 +1,28 @@
 //Winston nodejs logger
+const moment = require('moment-timezone');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
 
 loglevel = process.env.LOG_LEVEL;
+timezone = process.env.TZ;
+
+const appendTimestamp = format((info, opts) => {
+  if(opts.tz)
+    info.timestamp = moment().tz(opts.tz).format();
+  return info;
+});
 
 const logger = createLogger({
   level: loglevel,
   format: combine(
-    timestamp(),
+    appendTimestamp({ tz: timezone }),
     format.json()
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: '/app/logs/controller.log' })
+    new transports.File({
+      filename: '/app/logs/controller.log',
+    })
   ]
 });
 
