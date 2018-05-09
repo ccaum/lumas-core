7b5c6d394096a932b78f2fc8cd1efc4db2b95a84
Carl Caum <carl@carlcaum.com>
2018-05-08 22:00:28 -0700

    Remove erroneous commented out code

--- controller/homekit/ffmpeg.js
+++ controller/homekit/ffmpeg.js
@@ -141,20 +141,6 @@ FFMPEG.prototype.handleCloseConnection = function(connectionID) {
 }
 
 FFMPEG.prototype.handleSnapshotRequest = function(request, callback) {
-  //let resolution = request.width + 'x' + request.height;
-  //var imageSource = this.ffmpegImageSource !== undefined ? this.ffmpegImageSource : this.ffmpegSource;
-  //let ffmpeg = spawn('ffmpeg', (imageSource + ' -t 1 -s '+ resolution + ' -f image2 -').split(' '), {env: process.env});
-  //var imageBuffer = Buffer(0);
-  //this.logger.log("info", "Snapshot from " + this.name + " at " + resolution);
-  //this.logger.log("debug", 'ffmpeg '+imageSource + ' -t 1 -s '+ resolution + ' -f image2 -');
-  //ffmpeg.stdout.on('data', function(data) {
-  //  imageBuffer = Buffer.concat([imageBuffer, data]);
-  //});
-  //ffmpeg.on('close', function(code) {
-  //  if ( this.uploader )
-  //    { this.drive.storePicture(this.name,imageBuffer); }
-  //  callback(undefined, imageBuffer);
-  //}.bind(this));
   this.cameraConfig.getSnapshot( function(imgBuffer) {
     callback(undefined, imgBuffer);
   });
