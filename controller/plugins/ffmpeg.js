dcb6e24a80c24192200124224f799b3780a5459a
Carl Caum <carl@carlcaum.com>
2018-10-22 22:49:24 -0700

    Use ffmpeg to parse generic rtsp streams

--- controller/plugins/ffmpeg.js
+++ controller/plugins/ffmpeg.js
@@ -7,6 +7,7 @@ const Motion = require('motion-detect').Motion;
 const spawn = require('child_process').spawn;
 const ffmpeg = require('fluent-ffmpeg');
 const memfs = require('memfs');
+const fs = require('fs');
 const cv = require('/node_modules/opencv4nodejs/lib/opencv4nodejs');
 
 module.exports = { constructor, FFMPEG};
@@ -22,10 +23,10 @@ function FFMPEG(cameraName, config) {
   this.name = cameraName;
   this.streamURL = config.rtsp;
   this.frame;
-  this.frameDirectory = '/cameras/' + "test/";
+  this.frameDirectory = '/cameras/' + "test";
 
-  memfs.mkdirSync('/cameras/');
-  memfs.mkdirSync('/cameras/' + 'test');
+  if (!fs.existsSync('/cameras/')) fs.mkdirSync('/cameras/');
+  if (!fs.existsSync('/cameras/' + 'test')) fs.mkdirSync('/cameras/' + 'test');
 
   this.auth = {
     user: config.username,
@@ -39,17 +40,23 @@ function FFMPEG(cameraName, config) {
 util.inherits(FFMPEG, Camera);
 
 FFMPEG.prototype.processFrame = function (frame) {
+  const self = this;
+  logger.log('debug', "Processing frame " + frame);
   var file = this.frameDirectory + "/frame" + frame + ".jpg";
 
-  memfs.readFile(file, function(err, buf) {
+  fs.readFile(file, function(err, buf) {
     if (err) {
       logger.log('error', "Could not read frame file: " + err.message);
     }
 
-    this.emit('frame', buf);
+    self.emit('frame', buf);
   });
 
-  memfs.rm(file);
+  fs.unlink(file, function(err) {
+    if (err) {
+      logger.log('error', "Could not delete frame file " + file);
+    }
+  });
 }
 
 FFMPEG.prototype.processFeed = function () {
@@ -57,32 +64,36 @@ FFMPEG.prototype.processFeed = function () {
   var cameraOperation = retry.operation({ maxTimeout: 60 * 1000});
   var lastFrameEnqueued = 0;
 
-  ffmpeg(self.streamURL)
-  .outputOptions(['-rtsp_transport tcp', '-map 0:0', '-f image2'])
-  .on('start', function(commandLine) {
-    logger.log('debug', "Running ffmpeg with command: " + commandLine)
-  })
-  .on('progress', function(progress) {
-    var n = lastFrameEnqueued + 1;
-    lastFrameEnqueued = progress.frames - 1;
+  let ffmpegsh = spawn('ffmpeg', ['-i', self.streamURL, '-rtsp_transport', 'tcp', '-map', '0:0', '-f', 'image2', self.frameDirectory + '/frame%d.jpg'])
+  ffmpegsh.stdout.on('data', function(out) {
+    console.log('stdout');
+    console.log(out.toString('utf8'));
+  });
+  ffmpegsh.stderr.on('data', function(out) {
+    stderrString = out.toString('utf8')
+    let digit = 0
+    let regex = /^(frame)=\s+\d+\s/
+    let found = stderrString.match(regex)
+    if (found) {
+      string = found[0]
+      string = string.split(' ')
+      digit = string[string.length-2]
 
-    for (; n < progress.frames; n++) {
-      processFrame(n);
+
+      self.processFrame(digit)
     }
-  })
-  .on('stderr', function(err) {
-    logger.log('error', err);
-  })
-  .save(self.frameDirectory + '/frame%d.jpg')
-  //.save(self.frameDirectory + '/snapshot.jpg')
+  });
+  ffmpegsh.on('exit', function() {
+    console.log('ffmpeg closed');
+  });
 }
 
 FFMPEG.prototype.getSnapshot = function(callback) {
   const file = this.frameDirectory + '/' + snapshot.jpg;
 
-  memfs.readFile(file, function(err, buf) {
+  fs.readFile(file, function(err, buf) {
     if (err) {
-      logger.log("error", "Could not read " + file + " from memfs: " + err.message);
+      logger.log("error", "Could not read " + file + " from fs: " + err.message);
     }
 
     callback(buf);
